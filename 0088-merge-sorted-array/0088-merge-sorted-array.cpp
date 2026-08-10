d550fd8002c6aa9b938fcf8e971a92378524b0ab
Shristi Shukla <shristi.1587@gmail.com>
2026-08-10 12:30:43 +0530

    LeetCode 88: Merge Sorted Array | Two-pointer merge | Time: O(m+n), Space: O(m+n)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> nums;
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                nums.push_back(nums1[i]);
+                i++;
+            } else {
+                nums.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i < m) {
+            nums.push_back(nums1[i]);
+            i++;
+        }
+
+        while (j < n) {
+            nums.push_back(nums2[j]);
+            j++;
+        }
+
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = nums[k];
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
