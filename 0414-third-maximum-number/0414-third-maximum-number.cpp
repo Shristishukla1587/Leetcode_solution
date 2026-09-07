c5cc03eb19c372b44d55e072f57811a2c636c59c
Shristi Shukla <shristi.1587@gmail.com>
2026-09-07 16:43:34 +0530

    Time: 0 ms (100%), Space: 12.8 MB (93.86%) - LeetHub - Time: O(n), Space: O(1)

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -5,23 +5,24 @@ public:
         long long second = LLONG_MIN;
         long long third = LLONG_MIN;
         for(int x : nums){
-        if(x == first || x == second || x == third)
-          continue;
-        if(x > first){
-            third = second;
-            second = first;
-            first = x;
-        }
-        else if(x > second) {
-            third = second;
-            second = x;
+            if(x == first || x == second || x == third){
+                continue;
             }
-        else if(x > third){
-            third = x;
+            else if(x > first){
+                third = second;
+                second = first;
+                first = x;
+            }
+            else if(x > second){
+                third = second;
+                second = x;
+            }
+            else if(x > third){
+                third = x;
             }
         }
-              if(third == LLONG_MIN)
-                return first;
+        if(third <= LLONG_MIN)
+        return first;
 
         return third;
     }
