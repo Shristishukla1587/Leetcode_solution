9d1f798958121914e38bcba008bb42ce033b4277
Shristi Shukla <shristi.1587@gmail.com>
2026-09-11 14:19:49 +0530

    Time: 447 ms (5.01%), Space: 20.6 MB (83.86%) - LeetHub - Time: O(n^2), Space: O(1)

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < nums.size() - 1; i += 2) {
-            if(nums[i] != nums[i + 1]) {
-                return nums[i];
+        sort(nums.begin(),nums.end());
+        int n = nums.size(),k;
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
             }
+            if(count == 1)
+            return nums[i];
         }
-
-        return nums[nums.size() - 1];
+        return -1;
     }
 };
 
